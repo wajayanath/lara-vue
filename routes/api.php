6fda7e65e6d908a736d73231b0745a80c07c972d
wajayanath <wajayanath@gmail.com>
2019-03-30 15:08:53 +0530

    test with Travis ci

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('user', 'API\UserController');
-Route::get('profile','API\UserController@profile');
-Route::put('profile','API\UserController@updateProfile');
+Route::middleware(['auth:api','can:isAdmin'])->group(function () {
+    Route::apiResource('user', 'API\UserController');
+    Route::get('profile', 'API\UserController@profile');
+    Route::put('profile', 'API\UserController@updateProfile');
+});
